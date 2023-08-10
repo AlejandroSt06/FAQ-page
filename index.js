da1dc1800bd9fd05ca5bf86df3e45502d7108acd
Mirko <mirkosaavedra596@gmail.com>
2023-08-10 13:45:01 +0200

    add copyright

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const questions = $(".question");
+
+var data = new Date();
+var actualYear = data.getFullYear()
+document.getElementById("actual-year").innerHTML = actualYear
+
 var plus = true;
 questions.each(function(index, question) {
     const btn =$(question).find(".question-btn")
